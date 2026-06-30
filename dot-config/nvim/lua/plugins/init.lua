c7214f577595c35416a9e5d46170645a6db153f4
JohnC <108507081+Viper-31@users.noreply.github.com>
2026-06-30 22:56:12 +0800

    feat(nvim): Init nvim-lint

--- dot-config/nvim/lua/plugins/init.lua
+++ dot-config/nvim/lua/plugins/init.lua
@@ -42,6 +42,27 @@ return {
   },
 
   {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPost", "BufNewFile" },
+    config= function()
+      require("lint").linters_by_ft = {
+        javascript = { "eslint_d" },
+        typescript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+        python = { "ruff" },
+        sql = { "sqlfluff" },
+      }
+
+      vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+    end,
+  },
+
+  {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
