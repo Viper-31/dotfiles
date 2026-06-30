50177cccc5b8bb9b31a64403d32b4fb2e527c8a5
JohnC <108507081+Viper-31@users.noreply.github.com>
2026-06-30 20:03:45 +0800

    Unbind old nvchad cheatsheet

--- dot-config/nvim/lua/mappings.lua
+++ dot-config/nvim/lua/mappings.lua
@@ -3,11 +3,13 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local nomap = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- Cheatsheet
+nomap("n", "<leader>ch")
 map("n", "<leader>cs", "<cmd>NvCheatsheet<CR>", { desc = "toggle nvcheatsheet" })
 
 ----------------------------------------------------------------------------
