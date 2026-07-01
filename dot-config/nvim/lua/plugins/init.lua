127f49ed73f59401c227275d0bc686967b0d7627
JohnC <108507081+Viper-31@users.noreply.github.com>
2026-07-01 14:24:45 +0800

    Feat(nvim): Use telescope-fzf-native for fzf syntax

--- dot-config/nvim/lua/plugins/init.lua
+++ dot-config/nvim/lua/plugins/init.lua
@@ -84,4 +84,17 @@ return {
       },
     },
   },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      },
+    },
+  }
 }
